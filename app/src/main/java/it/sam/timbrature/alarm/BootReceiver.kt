fa62cc8da576e5fd91db98b161abdc6e79584c40
samu300987-create <samu300987@gmail.com>
2025-09-01 00:40:15 +0200

    Create app/src/main/java/it/sam/timbrature/alarm/BootReceiver.kt

---
+++ app/src/main/java/it/sam/timbrature/alarm/BootReceiver.kt
@@ -0,0 +1,5 @@
+package it.sam.timbrature.alarm
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+class BootReceiver:BroadcastReceiver(){ override fun onReceive(context: Context,intent: Intent){} }
