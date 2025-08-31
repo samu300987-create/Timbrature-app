ea715e7dc67b99e6eff94f9dec4d1b641b418c47
samu300987-create <samu300987@gmail.com>
2025-09-01 00:55:23 +0200

    Create BootReceiver.kt

---
+++ BootReceiver.kt
@@ -0,0 +1,11 @@
+package it.sam.timbrature.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Qui puoi ripianificare gli allarmi se necessario
+    }
+}
