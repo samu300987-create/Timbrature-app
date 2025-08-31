bb82d263d10b8fa8930382119d823f2126b7b553
samu300987-create <samu300987@gmail.com>
2025-09-01 00:54:52 +0200

    Create AlarmReceiver.kt

---
+++ AlarmReceiver.kt
@@ -0,0 +1,12 @@
+package it.sam.timbrature.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "È ora di timbrare!", Toast.LENGTH_SHORT).show()
+    }
+}
