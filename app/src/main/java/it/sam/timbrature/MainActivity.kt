589ee2a9d63e90f95ce2441b598acebd2f78199b
samu300987-create <samu300987@gmail.com>
2025-09-01 00:35:42 +0200

    Create app/src/main/java/it/sam/timbrature/MainActivity.kt

---
+++ app/src/main/java/it/sam/timbrature/MainActivity.kt
@@ -0,0 +1,4 @@
+package it.sam.timbrature
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+class MainActivity : ComponentActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) } }
