251d92afd0cfea34a9b644d1352eba757cfcb9b1
samu300987-create <samu300987@gmail.com>
2025-09-01 00:37:08 +0200

    Create app/src/main/java/it/sam/timbrature/model/Punch.kt

---
+++ app/src/main/java/it/sam/timbrature/model/Punch.kt
@@ -0,0 +1,4 @@
+package it.sam.timbrature.model
+import androidx.room.*
+@Entity(tableName="punches")
+data class Punch(@PrimaryKey(autoGenerate=true) val id:Long=0,val timestamp:Long,val type:String)
